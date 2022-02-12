a34ab4ec4df0caa596962a4102e9ee90fa5016b9
edwardmbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-02-12 19:24:04 +0300

    Add files via upload

---
+++ portfolio/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/edwardmbitsi/edwardmbitsi'
+      className='link footer__link'
+    >
+      Created By Edward Mbitsi
+    </a>
+  </footer>
+)
+
+export default Footer
